7cfda580ebb8fa3fe230c44047b169712bd90401
Daniel Jorge <danjorge@gmail.com>
2018-11-13 22:14:42 -0300

    adicionado configuracoes dos projetos

--- src/main/java/br/com/institutoatlantico/CoreApplication.java
+++ src/main/java/br/com/institutoatlantico/CoreApplication.java
@@ -1,9 +1,11 @@
-package br.com.instituloatlantico;
+package br.com.institutoatlantico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CoreApplication {
 
 	public static void main(String[] args) {
